0c0c6bc62bc98b74004908799d9a7ffcb3a777e1
OlTiatron <ostie1@yandex.com>
2023-09-21 00:14:26 +0400

    Заодно с гитом осваиваюсь

--- include/Ds18b20.h
+++ include/Ds18b20.h
@@ -21,10 +21,10 @@
 DeviceAddress tempDeviceAddress, insideThermometer, outsideThermometer;
 
 int numberOfDevices; //здесь будем хранить количество найденных устройств на шине
-/*
+
 // function печатает адрес найденного устройства
 void printAddress(DeviceAddress deviceAddress);
-
+/*
 // function to print считанную температуру
 void printTemperature(DeviceAddress deviceAddress);
 
